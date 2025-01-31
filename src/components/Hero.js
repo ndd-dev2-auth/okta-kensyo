77451e1c05acb5b7f06c63f2bc83a3527044b7b4
Kaito-Dokan <kaito.dokan@ctc-g.co.jp>
2025-01-31 16:33:48 +0900

    リンクだけ追加

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,6 +9,8 @@ const Hero = () => (
     <p className="lead">
       <a href="https://reactjs.org">React.js</a>を使ったOkta CIC検証のためのサンプルSPA
 
+      <a href="https://manage.cic-demo-platform.auth0app.com/dashboard/pi/moccasin-roadrunner-19215"
+        target="_blank" rel="noopener noreferrer">Okta管理画面へのリンク</a>
     </p>
 
   </div>
